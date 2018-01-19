841fbf046df2996030f703326c93d7a7dde851ae
zhaochaojian <zhaochaojian@meituan.com>
2018-01-19 11:38:31 +0800

    optimize P1103

--- P1103/main.cpp
+++ P1103/main.cpp
@@ -1,104 +1,68 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 
 using namespace std;
 
-struct Region {
-    int left, right;
-};
-
 struct SegmentTree {
     int left, right, count;
-    bool remove;
+    SegmentTree *lson, *rson;
 };
 
-void buidTree(vector<SegmentTree> &sgTree, int root, int l, int r) {
+SegmentTree *buildTree(int l, int r) {
     if (l > r) {
-        return;
+        return nullptr;
     }
     if (l == r) {
-        sgTree[root].remove = false;
-        sgTree[root].left = l;
-        sgTree[root].right = r;
-        sgTree[root].count = 1;
-        return;
+        auto *root = new SegmentTree{l, r, 1, nullptr, nullptr};
+        return root;
     }
-    sgTree[root].remove = false;
-    sgTree[root].left = l;
-    sgTree[root].right = r;
-    sgTree[root].count = r - l + 1;
+    auto *root = new SegmentTree{l, r, r - l + 1, nullptr, nullptr};
     int mid = (l + r) >> 1;
-    buidTree(sgTree, root * 2 + 1, l, mid);
-    buidTree(sgTree, root * 2 + 2, mid + 1, r);
+    root->lson = buildTree(l, mid);
+    root->rson = buildTree(mid + 1, r);
+    return root;
 }
 
-void upDate(vector<SegmentTree> &sgTree, int root, int regionLeft, int regionRight, int l, int r) {
-
-    if (l >= regionLeft && r <= regionRight) {
-        sgTree[root].count = 0;
-        sgTree[root].remove = true;
+void removeRegion(SegmentTree *root, int regionLeft, int regionRight) {
+    if (root->left >= regionLeft && root->right <= regionRight) {
+        root->count = 0;
         return;
     }
-    if (r < regionLeft || l > regionRight || sgTree[root].remove) {
+    if (root->right < regionLeft || root->left > regionRight) {
         return;
     }
-
-    int mid = (l + r) >> 1;
-    upDate(sgTree, root * 2 + 1, regionLeft, regionRight, l, mid);
-    upDate(sgTree, root * 2 + 2, regionLeft, regionRight, mid + 1, r);
-    if (sgTree[root * 2 + 1].remove && sgTree[root * 2 + 2].remove) {
-        sgTree[root].remove = true;
-    }
+    removeRegion(root->lson, regionLeft, regionRight);
+    removeRegion(root->rson, regionLeft, regionRight);
+    root->count = min(root->count, root->lson->count + root->rson->count);
 }
 
-void queryTree(vector<SegmentTree> sgTree, int root, int l, int r, int &removeTree) {
-    if (sgTree[root].remove) {
-        //removeTree += sgTree[root].count;
-        return;
-    }
-    if (l > r) {
-//        if (sgTree[root].remove) {
-//            removeTree += sgTree[root].count;
-//        }
-        return;
-    }
-    if (l == r) {
-        removeTree++;
-        return;
-    }
-
-    int mid = (l + r) >> 1;
-    queryTree(sgTree, root * 2 + 1, l, mid, removeTree);
-    queryTree(sgTree, root * 2 + 2, mid + 1, r, removeTree);
+void printTree(ofstream &fout, SegmentTree *root) {
+    if (root == nullptr)return;
+    fout << "left=" << root->left << "right=" << root->right << "count=" << root->count << endl;
+    printTree(fout, root->lson);
+    printTree(fout, root->rson);
 }
 
 int main() {
-    ifstream cin("a.in");
-    ofstream cout("a.out");
 
-    int l, m, i = 0, regionLeft, regionRight, removeTree = 0;
-    cin >> l >> m;
-    //vector<SegmentTree> sgTree(static_cast<unsigned long>(2 * l - 1), SegmentTree{0, 0, 0});
-    vector<SegmentTree> sgTree(200000, SegmentTree{0, 0, 0});
+    ifstream fin("a.in");
+    ofstream fout("a.out");
+
+    int l, m, i = 0, regionLeft, regionRight;
+    fin >> l >> m;
+    SegmentTree *root;
     //build segment tree
-    buidTree(sgTree, 0, 0, l);
+    root = buildTree(0, l);
 
+    //printTree(fout, root);
     for (i = 0; i < m; i++) {
-        //cin >> node[i].left >> node[i].right;
-        cin >> regionLeft >> regionRight;
-        upDate(sgTree, 0, regionLeft, regionRight, 0, l);
+        fin >> regionLeft >> regionRight;
+        removeRegion(root, regionLeft, regionRight);
     }
 
-//    i = 0;
-//    for (SegmentTree sg:sgTree) {
-//        cout << "key=" << i++ << "left=" << sg.left << "right=" << sg.right << "count=" << sg.count << "remove="
-//             << sg.remove << endl;
-//    }
-    queryTree(sgTree, 0, 0, l, removeTree);
-//
-    cout << removeTree;
-    cin.close();
-    cout.close();
+    fout << root->count;
+
+    fin.close();
+    fout.close();
     return 0;
 }
